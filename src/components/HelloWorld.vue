17ea7818f9314df122da14b54a6b3b0a37232da9
JuanfeRomero <juanferomerocode@gmail.com>
2021-11-26 11:05:15 -0300

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,238 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card
+          class="mx-auto"
+        >
+          <v-toolbar
+            color="teal"
+            dark
+          >
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            
+            <v-toolbar-title>Types</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </v-toolbar>
+
+          <v-list>
+            <v-list-group
+              v-for="item in items"
+              :key="item.name"
+            >
+            
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.name"></v-list-item-title>
+                </v-list-item-content>
+              </template>
+
+              <v-list-item
+                v-for="child in item.items"
+                :key="child.pokemon.name"
+              >
+                <v-list-item-content>
+                  <v-list-item-title v-text="child.pokemon.name"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-group>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card
+          class="mx-auto"
+        >
+          <v-treeview
+            rounded
+            hoverable
+            activatable
+            color="red"
+            :items="arbol"
+          >
+          </v-treeview>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card
+          class="mx-auto"
+        >
+          <v-list>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+
+            <v-list-group
+              :value="true"
+              prepend-icon="mdi-account-circle"
+            >
+              <template v-slot:activator>
+                <v-list-item-title>Users</v-list-item-title>
+              </template>
+
+              <v-list-group
+                :value="true"
+                no-action
+                sub-group
+              >
+                <template v-slot:activator>
+                  <v-list-item-content>
+                    <v-list-item-title>Admin</v-list-item-title>
+                  </v-list-item-content>
+                </template>
+
+                <v-list-item
+                  v-for="([title, icon], i) in admins"
+                  :key="i"
+                  link
+                >
+                  <v-list-item-title v-text="title"></v-list-item-title>
+
+                  <v-list-item-icon>
+                    <v-icon v-text="icon"></v-icon>
+                  </v-list-item-icon>
+                </v-list-item>
+              </v-list-group>
+
+              <v-list-group
+                no-action
+                sub-group
+              >
+                <template v-slot:activator>
+                  <v-list-item-content>
+                    <v-list-item-title>Actions</v-list-item-title>
+                  </v-list-item-content>
+                </template>
+
+                <v-list-item
+                  v-for="([title, icon], i) in cruds"
+                  :key="i"
+                  link
+                >
+                  <v-list-item-title v-text="title"></v-list-item-title>
+
+                  <v-list-item-icon>
+                    <v-icon v-text="icon"></v-icon>
+                  </v-list-item-icon>
+                </v-list-item>
+              </v-list-group>
+            </v-list-group>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from 'axios';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  export default {
+    name: 'HelloWorld',
+    data: () => ({
+      admins: [
+        ['Management', 'mdi-account-multiple-outline'],
+        ['Settings', 'mdi-cog-outline'],
+      ],
+      cruds: [
+        ['Create', 'mdi-plus-outline'],
+        ['Read', 'mdi-file-outline'],
+        ['Update', 'mdi-update'],
+        ['Delete', 'mdi-delete']
+              ],
+      items: [],
+      arbol: [
+        {
+          id: 1,
+          name: 'Applications :',
+          children: [
+            { id: 2, name: 'Calendar : app'},
+            { id: 3, name: 'Chrome : app'},
+            { id: 4, name: 'Webstorm : app'},
+          ],
+        },
+        {
+          id: 5,
+          name: 'Documents :',
+          children: [
+            {
+              id: 7,
+              name: 'src :',
+              children: [
+                { id: 8, name: 'index : ts' },
+                { id: 9, name: 'bootstrap : ts'},
+              ],
+            },
+          ],
+        },
+        {
+          id: 10,
+          name: 'material2 :',
+          children: [
+            { id: 12, name: 'v-btn : ts'},
+            { id: 13, name: 'v-card : ts'},
+            { id: 14, name: 'v-window : ts'},
+          ],
+        },
+        {
+          id: 15,
+          name: 'Downloads :',
+          children: [
+            { id: 16, name: 'October : pdf'},
+            { id: 17, name: 'November : pdf'},
+            { id: 18, name: 'Tutorial : html'},
+          ],
+        },
+        {
+          id: 19,
+          name: 'Videos :',
+          children: [
+            {
+              id: 20, name: 'Tutorials: ', children: [
+                { id: 21, name: 'Basic layouts : mp4'},
+                { id: 22, name: 'Advanced techniques : mp4'},
+                { id: 23, name: 'All about app: dir'},
+              ],
+            },
+            {
+              id: 24, name: 'Intro: mov'
+            },
+            {
+              id: 25, name: 'Conference introduction : avi',
+            },
+          ],
+        },
+      ],
+    }),
+    mounted() {
+      axios.get('https://pokeapi.co/api/v2/type')
+      .then(data => {
+        this.items = data.data.results;
+        this.items.forEach(element => {
+          element.items = [];
+          axios.get(element.url)
+          .then(internalData => {
+            element.items = internalData.data.pokemon;
+            console.log(element.items)
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        });
+        console.log(JSON.stringify(this.items, null, 2));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    },
+  }
+</script>
